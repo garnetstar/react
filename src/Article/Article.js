bc6c6b126b3ecf9a8b8b42a77f1364fae0284002
Jan Macháček <machacek.j@gmail.com>
2018-09-01 15:01:16 +0200

    fulltext hledani podle titlu

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ArticleDetail from './ArticleDetail';
-import { Link } from 'react-router-dom';
-import { Modal, ModalBody, ModalFooter, ModalHeader, Button } from 'reactstrap';
+import {Link} from 'react-router-dom';
+import {Modal, ModalBody, ModalFooter, ModalHeader, Button} from 'reactstrap';
 import AjaxHelperClass from "../ajaxHelper";
-import { Redirect } from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 
 
 class Article extends Component {
@@ -15,7 +15,7 @@ class Article extends Component {
 			articleId: props.articleId,
 			showNewArticleModal: false,
 			newArticleTitle: null,
-			newArticleId:null,
+			newArticleId: null,
 			ajaxHelper: AjaxHelperClass,
 		};
 		// this.setState({articleId: props.articleId});
@@ -26,10 +26,11 @@ class Article extends Component {
 		this.handleModalKeyPress = this.handleModalKeyPress.bind(this);
 		this.refModalInput = React.createRef();
 		this.refSearchInput = React.createRef();
+		this.handleArticleSearch = this.handleArticleSearch.bind(this)
 	}
 
 	componentDidMount() {
-		console.log( window.innerWidth);
+		console.log(window.innerWidth);
 		console.log(window.innerHeight);
 		this.reloadList();
 	}
@@ -68,20 +69,41 @@ class Article extends Component {
 
 	handleNewArticle(e) {
 		e.preventDefault();
-		this.setState({ showNewArticleModal: true }, function () {
-            this.refModalInput.current.focus();
-        });
+		this.setState({showNewArticleModal: true}, function () {
+			this.refModalInput.current.focus();
+		});
 
 	}
 
 	handleSaveArticle(e) {
-		const callback = function(res) {
+		const callback = function (res) {
 			res.json().then(json => {
 				this.setState({newArticleId: json.article_id});
 			});
 		}.bind(this);
 		this.state.ajaxHelper.articleAdd(this.state.newArticleTitle, callback);
 		this.toggle();
+	}
+
+	handleArticleSearch(e) {
+		// e.preventDefault();
+		var word = e.target.value;
+		var url;
+		if(word === '') {
+			url = '/api/article';
+		} else {
+			url = '/api/article/filter/title/' + word;
+		}
+		fetch(url)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log(result);
+					this.setState({articles: result});
+				}
+			);
+
+
 	}
 
 	toggle() {
@@ -92,40 +114,40 @@ class Article extends Component {
 
 	handleNewArticleTitle(e) {
 		// console.log('handle title' + e.target.value);
-		this.setState({ newArticleTitle: e.target.value });
+		this.setState({newArticleTitle: e.target.value});
 	}
 
 	componentWillReceiveProps(nextProps) {
-		this.setState({ articleId: nextProps.articleId });
-		if(!nextProps.articleId) {
+		this.setState({articleId: nextProps.articleId});
+		if (!nextProps.articleId) {
 			this.reloadList();
 		}
 		console.log('props ' + nextProps.articleId);
 	}
 
 	render() {
-		if(this.state.newArticleId !== null) {
+		if (this.state.newArticleId !== null) {
 			const url = '/article/edit/' + this.state.newArticleId;
-			return(<Redirect to={url} />);
+			return (<Redirect to={url}/>);
 		}
-		else if(this.state.error) {
-			return(<b>Error</b>);
-		} else if(this.state.isLoaded === true) {
-			return(
+		else if (this.state.error) {
+			return (<b>Error</b>);
+		} else if (this.state.isLoaded === true) {
+			return (
 				this.renderList()
 			);
 		} else {
-			return(
+			return (
 				<div>Loading...</div>
 			);
 		}
 	}
 
 	handleModalKeyPress(e) {
-		if(e.key === 'Enter') {
-            e.preventDefault();
-            // console.log(e.key);
-            this.handleSaveArticle(e);
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			// console.log(e.key);
+			this.handleSaveArticle(e);
 		}
 	}
 
@@ -134,52 +156,58 @@ class Article extends Component {
 
 		return(
 			<div>
-				<ArticleDetail articleId={this.state.articleId} />
+				<ArticleDetail articleId={this.state.articleId}/>
 			</div>);
 	}
 
 	renderList() {
 		let articles = this.state.articles;
 		const articleIdConst = parseInt(this.state.articleId, 10);
-		return(
+		return (
 			<div>
 				<br/>
-			<div className='row'>
-				<div className='col-sm-3'>
+				<div className='row'>
+					<div className='col-sm-3'>
 
-					<ul className='nav'>
-						<li className='nav-item'>
+						<ul className='nav'>
+							<li className='nav-item'>
 								<a className='nav-link' onClick={this.handleNewArticle} href='#'>New article</a>
-						</li>
-					</ul>
+							</li>
+						</ul>
 
-                    <div className='form-group'>
-                        <input ref={this.refSearchInput} type='text' className='form-control' />
-                    </div>
+						<div className='form-group'>
+							<input
+								ref={this.refSearchInput}
+								type='text'
+								className='form-control'
+								onChange={this.handleArticleSearch}
+							/>
+						</div>
 
-					<div className="list-group">
-						{articles.map((article, key)=>
-		           <Link key={key}
-								 to={`/article/${article.article_id}`}
-								 className={`list-group-item list-group-item-action ${article.article_id === articleIdConst ? ' active': ''}`}
-								 >
-								 {article.title}
-							 </Link>
-					 )}
+						<div className="list-group">
+							{articles.map((article, key) =>
+								<Link key={key}
+									  to={`/article/${article.article_id}`}
+									  className={`list-group-item list-group-item-action ${article.article_id === articleIdConst ? ' active' : ''}`}
+								>
+									{article.title}
+								</Link>
+							)}
+						</div>
+					</div>
+
+					<div className='col-sm-9'>
+
+						{!isNaN(articleIdConst) && (
+							<ArticleDetail articleId={articleIdConst}/>
+						)}
 					</div>
 				</div>
-				<div className='col-sm-9'>
-
-					{!isNaN(articleIdConst) && (
-							<ArticleDetail articleId={articleIdConst} />
-					)}
-				</div>
-			</div>
 
 
-			<Modal isOpen={this.state.showNewArticleModal} toggle={this.toggle} className={this.props.className}>
-				<form className='form-horizontal' >
-					<ModalHeader toggle={this.toggle}>New Article</ModalHeader>
+				<Modal isOpen={this.state.showNewArticleModal} toggle={this.toggle} className={this.props.className}>
+					<form className='form-horizontal'>
+						<ModalHeader toggle={this.toggle}>New Article</ModalHeader>
 						<ModalBody>
 							<div className='form-group'>
 								<label htmlFor='articleTitle'>Title</label>
@@ -189,20 +217,20 @@ class Article extends Component {
 									   className='form-control'
 									   onChange={this.handleNewArticleTitle}
 									   onKeyPress={this.handleModalKeyPress}
-									   value={this.newArticleTitle}	/>
+									   value={this.newArticleTitle}/>
 							</div>
 						</ModalBody>
 						<ModalFooter>
-						<Button color="primary" onClick={this.handleSaveArticle}>Create</Button>{' '}
-						<Button color="secondary" onClick={this.toggle}>Cancel</Button>
-				</ModalFooter>
-			</form>
-			</Modal>
+							<Button color="primary" onClick={this.handleSaveArticle}>Create</Button>{' '}
+							<Button color="secondary" onClick={this.toggle}>Cancel</Button>
+						</ModalFooter>
+					</form>
+				</Modal>
 
 
-
-		</div>
+			</div>
 		);
 	}
 }
+
 export default Article;
